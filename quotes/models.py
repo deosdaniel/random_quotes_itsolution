afd3aea448352531d5e110e79e0bb9bb6f392aee
daniel <deospr@gmail.com>
2025-08-25 21:25:34 +0300

    add blank=false to text and source in quote models

--- quotes/models.py
+++ quotes/models.py
@@ -3,11 +3,13 @@ from django.db import models
 # Create your models here.
 
 class Quote(models.Model):
-    source = models.CharField(max_length=100, null=False)
-    text = models.TextField(null=False)
+    source = models.CharField(max_length=100, blank=False ,null=False)
+    text = models.TextField(blank=False, null=False)
     date_added = models.DateTimeField(auto_now_add=True)
     views = models.IntegerField(default=0, null=False)
     likes = models.IntegerField(default=0, null=False)
 
+    def __str__(self):
+        return f'{self.source} | {self.text[:10]}'
 
     #weight = models.IntegerField(default=0)
