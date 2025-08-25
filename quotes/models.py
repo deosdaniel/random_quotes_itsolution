cbf5702749a58d51b54f890df9bc8c2a5323a653
daniel <deospr@gmail.com>
2025-08-25 21:04:33 +0300

    add initial model for quotes, add initial migrations

--- quotes/models.py
+++ quotes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Quote(models.Model):
+    source = models.CharField(max_length=100, null=False)
+    text = models.TextField(null=False)
+    date_added = models.DateTimeField(auto_now_add=True)
+    views = models.IntegerField(default=0, null=False)
+    likes = models.IntegerField(default=0, null=False)
+
+
+    #weight = models.IntegerField(default=0)
