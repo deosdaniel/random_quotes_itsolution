fd323019a08ab9a3bc4cfa806697b2dc4c00bf49
daniel <deospr@gmail.com>
2025-08-27 11:26:17 +0300

    Add weights, add form to add qoutes and validate the max 3 rule

--- quotes/urls.py
+++ quotes/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.quotes_list),
-    path('random_quote/', views.random_quote),
+    path('', views.quotes_list, name='quotes_list'),
+    path('random_quote/', views.random_quote, name='random_quote'),
+    path('add/', views.add_quote, name='add_quote' ),
 ]
--- quotes/views.py
+++ quotes/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.template.defaultfilters import random
+from django.db.models import F
 
+from .forms import QuoteForm
 from .models import Quote
+from .utils import get_random_quote
 
 
 # Create your views here.
@@ -13,9 +16,16 @@ def quotes_list(request):
     return render(request, 'quotes/quotes_list.html', {'quotes': quotes})
 
 
-
+def add_quote(request):
+    if request.method == 'POST':
+        form = QuoteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("add_quote")
+    else:
+        form = QuoteForm()
+    return render(request, 'quotes/add_quote.html', {'form': form})
 
 def random_quote(request):
-    q = Quote.objects.order_by('?').first()
-    context = {'quote': q}
-    return render(request,'quotes/random_quote.html', context)
+    q = get_random_quote()
+    return render(request,'quotes/random_quote.html', {'quote': q})
